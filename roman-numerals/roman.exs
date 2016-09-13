b69bfdd455de63692de129bc74f04c65fcfa373e
Brian Thomas Storti <btstorti@gmail.com>
2016-09-13 09:43:17 -0300

    Improve roman-numerals solution

--- roman-numerals/roman.exs
+++ roman-numerals/roman.exs
@@ -1,34 +1,35 @@
 defmodule Roman do
-
-  @romans [{1000, "M"},
-           {900, "CM"},
-           {500, "D"},
-           {400, "CD"},
-           {100, "C"},
-           {90, "XC"},
-           {50, "L"},
-           {40, "XL"},
-           {10, "X"},
-           {9, "IX"},
-           {5, "V"},
-           {4, "IV"},
-           {1, "I"}]
+  @romans [
+    {"M",  1000},
+    {"CM", 900},
+    {"D",  500},
+    {"CD", 400},
+    {"C",  100},
+    {"XC", 90},
+    {"L",  50},
+    {"XL", 40},
+    {"X",  10},
+    {"IX", 9},
+    {"V",  5},
+    {"IV", 4},
+    {"I",  1}
+  ]
 
   @doc """
   Convert the number to a roman number.
   """
   @spec numerals(pos_integer) :: String.t
   def numerals(number) do
-    numerals(number, "")
+    numerals(@romans, number, "")
   end
 
-  defp numerals(0, result), do: result
+  def numerals(_romans, 0, result), do: result
 
-  defp numerals(to_convert, result) do
-    {num, letter} = Enum.find(@romans, fn {numeral, roman} -> (to_convert / numeral) >= 1 end)
-    times = (to_convert / num) |> Float.floor |> round
-    remaining = rem(to_convert, num)
+  def numerals([{_letter, number} | t], to_convert, result) when to_convert < number do
+    numerals(t, to_convert, result)
+  end
 
-    numerals(remaining, result <> String.duplicate(letter, times))
+  def numerals([{letter, number} | _t] = list, to_convert, result) do
+    numerals(list, to_convert - number, result <> letter)
   end
 end
